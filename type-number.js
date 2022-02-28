8fcd4b957a3a11c504e279a037081c72b5553fa3
Victor Ramon <victoramon007@gmail.com>
2022-02-28 12:52:00 -0300

    Exemplos da Aula 1

---
+++ type-number.js
@@ -0,0 +1,12 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica);
+
+// Tipo 
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá Mundo!";
+const texto2 = 'Olá Mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Leonardo";
+
+console.log(citacao);
+
+// Concatenação (+)
+
+console.log(citacao + meuNome);
+
+// Template String OU Template Literal
+
