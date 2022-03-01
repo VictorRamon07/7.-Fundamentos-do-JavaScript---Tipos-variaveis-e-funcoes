ea0973b2328f6f476ce23073a9d3148727ab392b
Victor Ramon <victoramon007@gmail.com>
2022-03-01 14:42:33 -0300

    codigos var e conversores

---
+++ script.js
@@ -0,0 +1,40 @@
+/*
+Var
+*/
+
+// var altura = 5;
+// var comprimento = 7;
+
+// var area = altura * comprimento;
+
+// console.log(area);
+
+/* 
+Let
+*/
+
+// let forma = 'retângulo';
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retângulo') {
+//     area = altura * comprimento;
+// } else {
+//     area = (altura*comprimento) / 2;
+// }
+
+// console.log(area);
+
+const forma = 'retângulo';
+const altura = 5;
+const comprimento = 7;
+const area = altura*comprimento;
+
+if (forma === 'retângulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura*comprimento) / 2;
+}
+
+console.log(area);
