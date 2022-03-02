4e93e013497e6c8d8529be21656f984ba6d2dd75
Victor Ramon <victoramon007@gmail.com>
2022-03-02 12:33:47 -0300

    console e erros

---
+++ console.js
@@ -0,0 +1,22 @@
+//log -> registro
+
+const minhaVar = true;
+
+// console.log(123);
+// console.log("eu sou um texto");
+// console.log(minhaVar);
+
+//tratamento de erro!
+
+console.error('deu erro!');
+
+//console.table
+
+var people = [["John", "Smith"], ["Jane", "Doe"], ["Emily", "Jones"]]
+
+console.table(people);
+
+console.time('Temporizador');
+// Do a bunch of stuff.
+console.timeEnd('Temporizador');
+// Prints: bunch-of-stuff: 225.438ms
--- script.js
+++ script.js
@@ -26,15 +26,21 @@ Let
 
 // console.log(area);
 
-const forma = 'retângulo';
-const altura = 5;
-const comprimento = 7;
-const area = altura*comprimento;
+// const forma = 'retângulo';
+// const altura = 5;
+// const comprimento = 7;
+// const area = altura*comprimento;
 
-if (forma === 'retângulo') {
-    area = altura * comprimento;
-} else {
-    area = (altura*comprimento) / 2;
-}
+// if (forma === 'retângulo') {
+//     area = altura * comprimento;
+// } else {
+//     area = (altura*comprimento) / 2;
+// }
 
-console.log(area);
+// console.log(area);
+
+// Tipagem dinâmica
+// untyped
+
+let minhaVar = 567;
+minhaVar = "texto";
